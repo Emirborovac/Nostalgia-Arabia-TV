46222693fb1e0d67208ccb4aaf04c1913cd1e302
shehzaib <shehzaibbutt222@gmail.com>
2024-11-13 12:06:51 +0500

    Correctly display video on TV screen

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import SVG from "@/components/SVG";
+
+export default function Home() {
+  return (
+   <div className="relative">
+    <div>
+      <img src="/tv.jpg" alt="tv" className="w-[835.4px]" />
+      <SVG/>
+    </div>
+   
+   </div>
+  );
+}
